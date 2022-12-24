2fd328b314edff233934394e06258f1b42d5d320
mohammedthoufeek <www.thoufekabc@gmail.com>
2022-12-24 13:19:21 +0530

    first-commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './App.css';
+import Header from './Components/Header/header'
+
+const App=()=>{
+  return(
+    <>
+    <Header/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom'
+import App from './app'
+import React from 'react'
+import './index.css'
+
+ReactDOM.render(<App/>,document.querySelector("#root"))
